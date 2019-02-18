ed58d367ced792942d8e3da64498118ad9acd69e
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-18 01:46:44 -0600

    added main.sh placeholders for DEV and STAGE deployments

--- .travis/main.sh
+++ .travis/main.sh
@@ -1,6 +1,12 @@
 
 
-if [ $TRAVIS_PULL_REQUEST_BRANCH == "develop" ] && [ $TRAVIS_BRANCH == "stagging"]
+if [ $TRAVIS_BRANCH == "develop"]
+then
+
+    echo "DOING STUFF FOR DEVELOP BUILD AND DEPLOYMENT"
+
+# For pull reguest to stagging
+elif [ $TRAVIS_PULL_REQUEST_BRANCH == "develop" ] && [ $TRAVIS_BRANCH == "stagging"]
 then 
-   ./.travis/release-branch-create.sh 
+    echo "DEPLOY TO STAGGING"
 fi
