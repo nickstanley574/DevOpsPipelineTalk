d6f66d26c8cb1b77bb9d8430a77d317765baaf54
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-18 01:51:52 -0600

    fixed spacing error in if statments

--- .travis/main.sh
+++ .travis/main.sh
@@ -1,12 +1,12 @@
 
 
-if [ $TRAVIS_BRANCH == "develop"]
+if [ $TRAVIS_BRANCH == "develop" ]
 then
 
     echo "DOING STUFF FOR DEVELOP BUILD AND DEPLOYMENT"
 
 # For pull reguest to stagging
-elif [ $TRAVIS_PULL_REQUEST_BRANCH == "develop" ] && [ $TRAVIS_BRANCH == "stagging"]
+elif [ $TRAVIS_PULL_REQUEST_BRANCH == "develop" ] && [ $TRAVIS_BRANCH == "stagging" ]
 then 
     echo "DEPLOY TO STAGGING"
 fi
