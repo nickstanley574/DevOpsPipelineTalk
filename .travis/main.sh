ae19ec6577079eb08380dfb227f32cc24da8fda3
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-18 11:36:42 -0600

    added debug line to show travis env vars

--- .travis/main.sh
+++ .travis/main.sh
@@ -2,22 +2,22 @@
 set -e
 
 
+echo "DEBUG: TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
+
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
 
     echo "DEVELOP BUILD AND DEPLOYMENT"
 
-# For pull reguest to staging
 elif [ "$TRAVIS_PULL_REQUEST_BRANCH" == "develop" ] && [ "$TRAVIS_BRANCH" == "staging" ]
 then 
     echo "DEPLOY TO STAGGING"
     bash .travis/branch.sh "CREATE"
 
-# For pull reguest to master
 elif [ "$TRAVIS_PULL_REQUEST_BRANCH" == "staging" ] && [ "$TRAVIS_BRANCH" == "master" ]
 then 
     echo "DEPLOY TO MASTER"
     bash .travis/branch.sh "RELEASE"
 else 
-    echo "ELSE: TRAVIS_BRANCH=$TRAVIS_BRANCH"
+    echo "ELSE: TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH"
 fi
