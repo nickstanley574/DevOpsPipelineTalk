7337c0a36cd50632dc9fe601c831be6284c75e0d
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-18 01:55:39 -0600

    added quotes around variables in if statements

--- .travis/main.sh
+++ .travis/main.sh
@@ -1,12 +1,13 @@
+# exit when any command fails
+set -e
 
-
-if [ $TRAVIS_BRANCH == "develop" ]
+if [ "$TRAVIS_BRANCH" == "develop" ]
 then
 
     echo "DOING STUFF FOR DEVELOP BUILD AND DEPLOYMENT"
 
 # For pull reguest to stagging
-elif [ $TRAVIS_PULL_REQUEST_BRANCH == "develop" ] && [ $TRAVIS_BRANCH == "stagging" ]
+elif [ "$TRAVIS_PULL_REQUEST_BRANCH" == "develop" ] && [ "$TRAVIS_BRANCH" == "stagging" ]
 then 
     echo "DEPLOY TO STAGGING"
 fi
