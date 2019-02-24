2f857821ecc2a18237d25141e796390d9e1210b5
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 08:51:38 -0600

    more build debugging

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,18 +5,19 @@ set -x
 echo "Building ..."
 cd ..
 pwd
+ls -al
 
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
-    $TAG1='develop'
-    $TAG2=$(.travis/version.sh)
+    TAG1='develop'
+    TAG2=$(.travis/version.sh)
 else
-    $TAG1=$TRAVIS_COMMIT
-    $TAG2=$TRAVIS_BUILD_ID
+    TAG1=$TRAVIS_COMMIT
+    TAG2=$TRAVIS_BUILD_ID
 if
 
 echo "Build docker image:"
-docker build -t $TAG1 -t $TAG2
+docker build -t $TAG1 -t $TAG2 .
 
 echo "Push Dockerbuild to dockerhub"
 echo "$DOCKER_PASSWORD_TRAVIS" | docker login -u "$DOCKER_USERNAME_TRAVIS" --password-stdin
