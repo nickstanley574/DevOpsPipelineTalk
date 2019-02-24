3b52cad520d6317a5321217e4679b8bbff391677
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 12:29:17 -0600

    simplified build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,10 +3,6 @@ set -e
 set -x
 
 echo "Building ..."
-pwd
-ls -al
-
-git status 
 
 git log -n 5 --pretty=format:"%h - %an, %ar : %s"
 
@@ -17,19 +13,15 @@ then
     ENVTAG='develop'
 fi
 
-echo "$DOCKER_PASSWORD_TRAVIS" | docker login -u "$DOCKER_USERNAME_TRAVIS" --password-stdin
-
-DOCKERRPO="nickstanley574/pipeline-demo-protoype"
-TAGSTR=""
 echo "Build docker image:"
+DOCKERRPO="nickstanley574/pipeline-demo-protoype"
+docker build -t "buildimage" .
+
+IMAGE_ID=$(docker images -q buildimage)
+
+echo "Push Dockerbuild to hub.docker.com"
+echo "$DOCKER_PASSWORD_TRAVIS" | docker login -u "$DOCKER_USERNAME_TRAVIS" --password-stdin
 for tag in {$BUILDTAG,$ENVTAG}; do
-    TAGSTR+="-t $DOCKERRPO:$tag "
+    docker tag $IMAGE_ID $DOCKERRPO:$tag
+    docker push $DOCKERRPO:$tag
 done
-
-docker build $TAGSTR .
-
-echo "Push Dockerbuild to dockerhub"
-docker push nickstanley574/pipeline-demo-protoype:${tag}
-
-
-echo "Done."
