4b627ac917ddc1f4fe11d8ff1bc6802acd4e80fb
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 20:50:05 -0600

    debug true heroku

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ set -x
 
 docker logout
 docker login -u "$HEROKU_USER" -p "$HEROKU_KEY" registry.heroku.com
-heroku container:login
+HEROKU_DEBUG=true HEROKU_DEBUG_HEADERS=1 heroku container:login
 
 
 
