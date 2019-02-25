eddbb91625fe89cbfa023c0a7e6168466ff779bd
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 19:32:13 -0600

    I don't care any more

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -2,12 +2,6 @@
 
 set -x
 
-heroku --version
-sleep 10
-heroku config:set HEROKU_API_KEY=$HEROKU_TOKEN_DEVELOP --app 'develop-simplelookup'
-sleep 10
-heroku apps
-
 echo "test 1:"
 sleep 1
 echo "PASSED"
