8bbf67b2e14951af2b302a46fd00d3d03d9a99a6
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 18:48:23 -0600

    starting to grasp and straws

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -3,9 +3,9 @@
 set -x
 
 heroku --version
-sleep 1
-heroku config:set HEROKU_API_KEY=$HEROKU_TOKEN_DEVELOP
-sleep 1
+sleep 10
+heroku config:set HEROKU_API_KEY=$HEROKU_TOKEN_DEVELOP --app 'develop-simplelookup'
+sleep 10
 heroku apps
 
 echo "test 1:"
