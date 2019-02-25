9b84445675079bd78f3d51cb5bd1875dce07cc8e
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-24 21:00:59 -0600

    netrc

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,8 @@ echo "deploying ..."
 set -e
 set -x 
 
+cat ~/.netrc 
+
 docker logout
 docker login -u "$HEROKU_USER" -p "$HEROKU_KEY" registry.heroku.com
 HEROKU_DEBUG=true HEROKU_DEBUG_HEADERS=1 heroku container:login
