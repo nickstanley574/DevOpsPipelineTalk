efb023c41df907931e6a2c093386e2dbd69e6b22
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-26 09:12:05 -0600

    added coding standards dummy/mockup

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
+set -e
+
 cat .travis/display/test
 
-cat .travis/displaytest-unit
-
+cat .travis/display/test-unit
 echo "----------------------------------------------------------------------"
 echo "Ran 22 tests in 0.221s"
-
 echo "OK"
+echo
+
+cat .travis/display/test-code-standards
+echo "----------------------------------------------------------------------"
+echo "88% > 85% minimum"
+echo "Passed"
+echo
