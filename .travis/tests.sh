908e389632e140d3c4ad8f187497818ffb8df175
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-26 09:29:23 -0600

    added underline to test headers

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -5,19 +5,16 @@ set -e
 cat .travis/display/test
 
 cat .travis/display/test-unit
-echo "----------------------------------------------------------------------"
 echo "Ran 22 tests in 0.221s"
 echo "OK"
 echo
 
 cat .travis/display/test-code-standards
-echo "----------------------------------------------------------------------"
 echo "88% > 85% minimum"
 echo "Passed"
 echo
 
 cat .travis/display/test-acceptacne
-echo "----------------------------------------------------------------------"
 echo "Project Manger approved"
 echo "Passed"
 echo
