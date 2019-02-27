f08356bc56d0e2e02bd8dd733afa68eca3fddaf7
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-02-27 00:55:03 -0600

    updated landing page with image and env info

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,6 +10,15 @@ then
     ENVTAG='develop'
 fi
 
+cat >build.json <<EOF
+{
+  "travis_build_web_url": $TRAVIS_BUILD_WEB_URL,
+  "travis_build_id": $TRAVIS_BUILD_ID,
+  "travis_job_web_url": $TRAVIS_JOB_WEB_URL,
+  "travis_job_id": $TRAVIS_JOB_ID"
+}
+EOF
+
 BUILDTAG="build-$TRAVIS_BUILD_NUMBER"
 DOCKERRPO="nickstanley574/uicpipeline"
 docker build --quiet -t "buildimage" .
@@ -20,3 +29,4 @@ for tag in {$BUILDTAG,$ENVTAG}; do
     docker tag $IMAGE_ID $DOCKERRPO:$tag
     docker push $DOCKERRPO:$tag
 done
+
--- local-build.sh
+++ local-build.sh
@@ -6,6 +6,14 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 tag=$(echo "$branch" | tr / -)
 image="local/uicpipeline/$tag"
 
-.travis/tests.sh
+cat >build.json <<EOF
+{
+  "travis_build_web_url": "Hello World",
+  "travis_build_id": "123456789",
+  "travis_job_web_url": "Hello World2",
+  "travis_job_id": "987654321"
+}
+EOF
+
 docker build -t "$image" .
 BRANCH=$image docker-compose -f local-docker-compose.yml up
